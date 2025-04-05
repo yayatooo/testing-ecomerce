ca6cff2f53f2abd99cf9f8f869bf5a90c72a1f93
Rahmat Hidayat <yayatbx@gmail.com>
2025-04-05 14:51:27 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use Bun official image
+FROM oven/bun:latest
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and bun.lockb first (for caching dependencies)
+COPY package.json bun.lockb ./
+
+# Install dependencies
+RUN bun install --frozen-lockfile
+
+# Copy the rest of the project files
+COPY . .
+
+# Expose the application port (adjust as needed)
+EXPOSE 3000
+
+# Run the application
+CMD ["bun", "start"]
---
+++ prisma/migrations/20250226095911_init/migration.sql
@@ -0,0 +1,41 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "avatarUrl" TEXT,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "onBanned" BOOLEAN NOT NULL DEFAULT false,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationCode" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VerificationCode_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationCode_code_key" ON "VerificationCode"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationCode_userId_key" ON "VerificationCode"("userId");
+
+-- AddForeignKey
+ALTER TABLE "VerificationCode" ADD CONSTRAINT "VerificationCode_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
